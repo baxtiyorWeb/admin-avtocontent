827787046d3b9be921c9dc64b86820f62a69045e
baxtiyorqurbonnazarov33 <baxtiyorqurbonnazarov33@gmail.com>
2025-07-25 17:16:08 +0500

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,13 @@ import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
+
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "AvtoKontinent.uz",
+  description: "AvtoKontinent uz saytiga hush kelibsiz",
+  generator: "AvtoKontinent",
+};
+
 
 export default function RootLayout({
   children,
--- lib/api-client.ts
+++ lib/api-client.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://avtokontinent-uz.onrender.com/api";
+const API_BASE_URL = "http://127.0.0.1:8000/api";
 
 export async function fetcher<T>(
   url: string,
